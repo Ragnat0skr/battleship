ae15928a342984a2f37b1ed8cdf64150096106ad
Ragnat0skr <43636939+Ragnat0skr@users.noreply.github.com>
2019-06-05 14:51:24 +0100

    Add files via upload

---
+++ Sea.java
@@ -0,0 +1,12 @@
+import javafx.scene.shape.Rectangle;
+
+public class Sea extends Rectangle{
+
+	int x; // Each sea tile has 2 main propeties, x and y placements
+	int y; // Other propeties will be determined in main using javafx
+	
+	public Sea(){
+
+	}
+	
+}
